1e9466089d8ba4e261d76f222c70af710ad61a42
Memphis Meng <1310750076@qq.com>
2020-07-28 19:12:06 -0400

    add async property

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
-from pyquery import PyQuery as pq
 import urllib
 import requests
+import scrapy
+from scrapy.crawler import CrawlerProcess, CrawlerRunner
+from twisted.internet import reactor
 from PIL import Image
 from requests.exceptions import MissingSchema
 from best_rated_promotion import promote
 from io import BytesIO
 
-list_of_genres = ['animation', 'western', 'fantasy', 'thriller', 'drama', 'history', 'crime', 'comedy',
-                          'tv movie', 'documentary', 'mystery', 'adventure', 'family', 'romance', 'action', 'horror',
-                          'war', 'music', 'science fiction', 'foreign']
+list_of_genres = ['animation', 'western', 'fantasy', 'thriller', 'drama', 'history', 'crime', 'comedy', 'tv movie',
+                  'documentary', 'mystery', 'adventure', 'family', 'romance', 'action', 'horror', 'war', 'music',
+                  'science fiction', 'foreign']
 
 movies = pd.read_csv('movies.csv')
 movies.drop_duplicates(inplace=True)
@@ -57,72 +59,86 @@ def display():
         st.write()
 
 
-# streamlit app design
-st.header('Welcome to MovieLens Recommendation System!')
-st.sidebar.header('Please enter your User ID:')
-id_ = st.sidebar.number_input('Your ID')
-st.sidebar.subheader('New User Only!')
-default = st.sidebar.radio("Do you want our default recommendation?", ('Yes', 'No'))
-options = st.sidebar.multiselect('Your Choice of Genres', list_of_genres)
-button = st.sidebar.button('Confirm')
+class Spider(scrapy.Spider):
+    def __init__(self, codes):
+        self.name = 'rec'
+        self.allowed_domains = ['themoviedb.org']
+        self.start_urls = ['https://www.themoviedb.org/movie/']
+        self.posters = []
+        self.codes = codes
 
-films, posters = [], []
-if button:
-    if int(id_) in ratings.userId.unique():
-        recommended_movies = recommender(int(id_))
-        for j in range(10):
-            try:
-                films.append(recommended_movies['title'].iloc[j])
-                doc = pq('https://www.themoviedb.org/movie/' + str(recommended_movies['movieId'].iloc[j]))
-                image = doc('.image_content.backdrop img').attr('data-src')
-                poster = requests.get(image)
-                posters.append(poster)
-            except IndexError:
-                print('Movie\'s title is not found in the database')
-            except urllib.error.HTTPError as exception:
-                print('Movie\'s URL is broken!')
-                posters.append(404)
-            except MissingSchema as exception:
-                print('Movie\'s poster is not found!')
-                posters.append(404)
+    def start_requests(self):
+        for code in self.codes:
+            scrapy.Request(url=self.start_urls[0] + str(int(code)),
+                           callback=self.parse, dont_filter=True)
 
-        display()
+    def parse(self, response):
+        try:
+            link = response.xpath('//div[@class="image_content backdrop"]/img/@data-src').extract_first(default='')
+            poster = requests.get(link)
+            self.posters.append(poster)
+        except IndexError:
+            print('Movie\'s title is not found in the database')
+        except urllib.error.HTTPError as exception:
+            print('Movie\'s URL is broken!')
+            poster = 404
+            self.posters.append(poster)
+        except MissingSchema as exception:
+            print('Movie\'s poster is not found!')
+            poster = 404
+            self.posters.append(poster)
 
-    # newcomer
-    else:
-        # customized choice
-        if default == 'No':
-            rates = dict()
-            for option in list_of_genres:
-                if option in options:
-                    rates[option] = [user_profile.mean(axis=0)[option]]
-                else:
-                    rates[option] = [0]
 
-            preference = pd.DataFrame(rates, index=[int(id_)]).T.sort_index(axis=0)
-            test_list = np.dot(TFIDF, preference)
-            print(test_list.shape)
-            movie_ranks = pd.DataFrame(data=test_list, index=movie_profile['movieId'].unique(), columns=[int(id_)])
-            print(movie_ranks)
-            recommended_movies = pd.merge(movie_ranks, movies, left_on=movie_ranks.index, right_on='id') \
-                                     .sort_values(by=int(id_), ascending=False, axis=0).iloc[0:10][['id', 'title']]
+if __name__ == '__main__':
+    # streamlit app design
+    st.header('Welcome to MovieLens Recommendation System!')
+    st.sidebar.header('Please enter your User ID:')
+    id_ = st.sidebar.number_input('Your ID')
+    st.sidebar.subheader('New User Only!')
+    default = st.sidebar.radio("Do you want our default recommendation?", ('Yes', 'No'))
+    options = st.sidebar.multiselect('Your Choice of Genres', list_of_genres)
+    button = st.sidebar.button('Confirm')
 
-        # default recommendation
-        elif default == 'Yes':
-            recommended_movies = promote()
+    films, posters = [], []
+    if button:
+        # existed users
+        if int(id_) in ratings.userId.unique():
+            recommended_movies = recommender(int(id_))
+            films = recommended_movies['title'].values.tolist()
+            runner = CrawlerRunner()
+            runner.crawl(Spider, codes=recommended_movies['movieId'])
+            d = runner.join()
+            d.addBoth(lambda _: reactor.stop())
+            reactor.run()
+            posters = runner.spiders.posters
+            display()
 
-        for i in range(10):
-            try:
-                films.append(recommended_movies['title'].iloc[i])
-                doc = pq('https://www.themoviedb.org/movie/' + str(recommended_movies['id'].iloc[i]))
-                image = doc('.image_content.backdrop img').attr('data-src')
-                poster = requests.get(image)
-                posters.append(poster)
-            except urllib.error.HTTPError as exception:
-                print('Movie\'s link is broken!')
-                posters.append(404)
-            except MissingSchema as exception:
-                print('Movie\'s poster is not found!')
-                posters.append(404)
+        # newcomer
+        else:
+            # customized choice
+            if default == 'No':
+                rates = dict()
+                for option in list_of_genres:
+                    if option in options:
+                        rates[option] = [user_profile.mean(axis=0)[option]]
+                    else:
+                        rates[option] = [0]
 
-        display()
+                preference = pd.DataFrame(rates, index=[int(id_)]).T.sort_index(axis=0)
+                test_list = np.dot(TFIDF, preference)
+                movie_ranks = pd.DataFrame(data=test_list, index=movie_profile['movieId'].unique(), columns=[int(id_)])
+                recommended_movies = pd.merge(movie_ranks, movies, left_on=movie_ranks.index, right_on='id') \
+                                         .sort_values(by=int(id_), ascending=False, axis=0).iloc[0:10][['id', 'title']]
+
+            # default recommendation
+            elif default == 'Yes':
+                recommended_movies = promote()
+
+            films = recommended_movies['title'].values.tolist()
+            runner = CrawlerRunner()
+            runner.crawl(Spider, codes=recommended_movies['id'])
+            d = runner.join()
+            d.addBoth(lambda _: reactor.stop())
+            reactor.run()
+            posters = runner.spiders.posters
+            display()
